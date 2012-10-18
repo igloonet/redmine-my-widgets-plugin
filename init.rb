00db61eca9520135d5c23c254e0cd7d21c5217a6
Marek Hulan <ares@igloonet.cz>
2012-10-18 10:43:56 +0200

    Compatibility with 2.1

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_my_widgets do
+Redmine::Plugin.register :redmine_my_widgets_plugin do
 	name 'Redmine My Widgets plugin'
 	author 'Brad Beattie'
 	description 'This plugin provides instances of Redmine with additional widgets for My page.'
